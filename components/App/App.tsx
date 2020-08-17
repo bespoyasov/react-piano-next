5122b40b2e0a81281663bda8d398fc7bfffaa850
Alexander Bespoyasov <forjob159@gmail.com>
2020-08-17 13:22:33 +0300

    Turn off SSR on the main page

--- components/App/App.tsx
+++ components/App/App.tsx
@@ -1,15 +1,20 @@
 import React from "react";
+import dynamic from "next/dynamic";
+
 import { Footer } from "../Footer";
 import { Logo } from "../Logo";
-import { Main } from "../Main";
 import styles from "./App.module.css";
+
+const DynamicMain = dynamic(() => import("../Main").then((mod) => mod.Main), {
+  ssr: false,
+});
 
 export const App: React.FC = () => {
   return (
     <div className={styles.app}>
       <Logo />
       <main className={styles.content}>
-        <Main />
+        <DynamicMain />
       </main>
       <Footer />
     </div>
